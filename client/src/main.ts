e3ed9ee5ed49b50dc9fe9706fdaec3a9c2fca97e
cwzp990 <cwzp990@163.com>
2023-05-08 12:18:51 +0800

    feat(img): load img to storage

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
+
 import './style.css'
 import App from './App.vue'
+import { ImgUtil } from './utils/imgUtil'
+
+ImgUtil.init() // 将本地图片导入storage内存中
 
 createApp(App).mount('#app')
---
+++ client/src/utils/imgUtil.ts
@@ -0,0 +1,29 @@
+import goodStorage from 'good-storage'
+
+export class ImgUtil {
+  static imgList: Record<string, string> = {}
+
+  static init() {
+    this.imgList = goodStorage.get('imgList', {})
+    if (this.isEmpty()) {
+      this.loadAllImg()
+      goodStorage.set('imgList', this.imgList)
+    }
+  }
+
+  static isEmpty() {
+    return Object.keys(this.imgList).length === 0
+  }
+
+  static loadAllImg() {
+    const imgMap = import.meta.glob('../assets/img/**/*.png', { eager: true })
+    let absolutePath = ''
+    let imgName = ''
+    Object.keys(imgMap).forEach((relativePath) => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      absolutePath = (imgMap[relativePath] as any).default
+      imgName = absolutePath.substring(absolutePath.lastIndexOf('/') + 1)
+      this.imgList[imgName] = absolutePath
+    })
+  }
+}
