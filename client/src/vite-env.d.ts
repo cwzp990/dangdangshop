aaf84deda2cc9bde317a21be2b6fe8b0818d65b6
cwzp990 <cwzp990@163.com>
2023-05-08 11:18:41 +0800

    feat(vite): env variable

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,3 +1,10 @@
 /// <reference types="vite/client" />
 
 // https://cn.vitejs.dev/guide/env-and-mode.html#env-files
+
+interface ImportMetaEnv {
+  VITE_HOST: string
+  VITE_PORT: number
+  VITE_BASE_URL: string
+  VITE_PROXY_DOMAIN: string
+}
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,35 @@
+import fs from 'fs'
+import dotenv from 'dotenv'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+// export default defineConfig({
+//   plugins: [vue()]
+// })
+
+export default defineConfig((env) => {
+  let server = {}
+  const envFileName = `.env.${env.mode}`
+  const envData = fs.readFileSync(envFileName)
+  const envConf = dotenv.parse(envData)
+
+  if (env.mode === 'development') {
+    server = {
+      host: envConf.VITE_HOST,
+      port: envConf.VITE_PORT,
+      proxy: {
+        [envConf.VITE_BASE_URL]: {
+          target: envConf.VITE_PROXY_DOMAIN
+        }
+      }
+    }
+  }
+  if (env.mode === 'production') {
+    // production
+  }
+  return {
+    server,
+    plugins: [vue()]
+  }
 })
